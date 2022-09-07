380ffb931f83cb2d056de029b189ca1b7540872c
DiametrFQ <hohlov.03@inbox.ru>
2022-09-07 15:10:58 +0300

    true O

--- dist/labaNop.js
+++ dist/labaNop.js
@@ -7,8 +7,8 @@ class Queue {
     _N_op;
     constructor(quantity) {
         this._Array = [];
-        this._F_lim = Math.floor(1 + (11 + Math.log2(quantity) * quantity * 16) * (quantity - 1));
-        this._O_F_lim = Math.floor(Math.log2(quantity) * quantity ** 2);
+        this._F_lim = Math.floor(1 + (11 + Math.log2(quantity) * 16) * (quantity - 1));
+        this._O_F_lim = Math.floor(Math.log2(quantity) * quantity);
         this._T_lim = 0;
         this._N_op = 0;
     }
@@ -35,20 +35,20 @@ class Queue {
             const A2 = this.Shift();
             this._N_op += 1;
             const limit = A1.length + A2.length;
-            this._N_op += 4;
+            this._N_op += 3;
             let retA = [];
-            this._N_op += 2;
+            this._N_op += 1;
             for (let i = 0; i < limit; i++) {
                 if (A1.length > 0 && A2.length > 0) {
-                    this._N_op += 8;
+                    this._N_op += 7;
                     A1 > A2 ? retA.push(A2.shift()) : retA.push(A1.shift());
                 }
                 else if (A2.length === 0) {
-                    this._N_op += 4;
+                    this._N_op += 3;
                     retA.push(A1.shift());
                 }
                 else if (A1.length === 0) {
-                    this._N_op += 4;
+                    this._N_op += 3;
                     retA.push(A2.shift());
                 }
             }
@@ -58,7 +58,7 @@ class Queue {
         this._T_lim = performance.now();
     }
 }
-for (let i = 60; i < 601; i += 60) {
+for (let i = 60; i < 6061; i += 60) {
     let Quantity = i;
     let queue = new Queue(Quantity);
     for (let i = 0; i < Quantity; i++)
@@ -68,6 +68,7 @@ for (let i = 60; i < 601; i += 60) {
     console.log("C2 = " + queue._O_F_lim / queue._T_lim);
     console.log("C3 = " + queue._F_lim / queue._N_op);
     console.log("C4 = " + queue._O_F_lim / queue._N_op);
+    console.log("OP = " + 1 / (queue._O_F_lim / queue._N_op));
     console.log();
 }
 //# sourceMappingURL=labaNop.js.map
